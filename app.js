f20f39d4bd15325a5115ea009260c23bea2c9d75
Suraj jaiswal <surajaiswal13@gmail.com>
2022-02-19 21:33:38 +0530

    Partial compose feature

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 })
 
+app.post("/compose", (req, res) => {
+  console.log(req.body.composeText)
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
