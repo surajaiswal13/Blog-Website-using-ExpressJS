c741b0e6286b6266a46d513963665e87e4f95bf8
Suraj jaiswal <surajaiswal13@gmail.com>
2022-02-20 20:04:18 +0530

    Added Individual Post Functionality

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const _ = require("lodash");
+
 let posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -47,17 +49,23 @@ app.post("/compose", (req, res) => {
 })
 
 app.get("/posts/:postName", (req, res) => {
-  const requestedTitle = req.params.postName;
+  const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.foEach((post) => {
-    const storedTitle = post.title;
+  for(let i = 0; i<posts.length; i++) {
+    const storedTitle = _.lowerCase(posts[i].title);
 
     if (storedTitle === requestedTitle) {
-      console.log("Match Found!");
+      // console.log("Match Found!");
+      res.render("post", {"postDetail": posts[i]})
+    } else{
+      console.log("Not a Match !");
     }
-  })
+  };
+  // posts.foEach(function(post) {
+  //
+  // });
 
-})
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
