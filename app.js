453e1a46b57f7f580e8e5a22e748c02173b21596
Suraj jaiswal <surajaiswal13@gmail.com>
2022-02-22 02:45:27 +0530

    Cloud Database Added, Compose DB Functionality

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const mongoose = require("mongoose");
+
 const _ = require("lodash");
 
-let posts = [];
+mongoose.connect("mongodb+srv://admin-suraj:suraj1234@cluster0.ckltw.mongodb.net/blogDB", {useNewUrlParser: true});
+
+const blogSchema = {
+  title: String,
+  content: String
+}
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+
+// let posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -20,7 +32,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {"homeContent": homeStartingContent, "posts":posts});
+
+  Blog.find({}, function(err, posts) {
+    if (!err){
+      console.log(posts);
+      res.render("home", {"homeContent": homeStartingContent, "posts":posts});
+    } else {
+      console.log(err);
+    }
+  })
+
 });
 
 app.get("/about", (req, res) => {
@@ -37,30 +58,48 @@ app.get("/compose", (req, res) => {
 
 app.post("/compose", (req, res) => {
 
-  const post = {
+  // const post = {
+  //   title: req.body.postTitle,
+  //   body : req.body.postBody,
+  // }
+
+  const post = new Blog({
     title: req.body.postTitle,
-    body : req.body.postBody,
-  }
+    content: req.body.postBody,
+  })
 
-  posts.push(post);
+  post.save(function(err) {
+    if (!err){
+      res.redirect("/")
+    }
+  })
+  // posts.push(post);
 
-  res.redirect("/")
+  // res.redirect("/")
 
 })
 
 app.get("/posts/:postName", (req, res) => {
-  const requestedTitle = _.lowerCase(req.params.postName);
+  // console.log(req.params.postName);
+  // const requestedTitle = _.lowerCase(req.params.postName);
+  const requestedTitle = req.params.postName;
+  // console.log(requestedTitle);
 
-  for(let i = 0; i<posts.length; i++) {
-    const storedTitle = _.lowerCase(posts[i].title);
+  Blog.find({}, (err, foundBlogs) => {
+    console.log(foundBlogs);
+    res.render("post", {"postDetail": foundBlogs})
+  })
 
-    if (storedTitle === requestedTitle) {
-      // console.log("Match Found!");
-      res.render("post", {"postDetail": posts[i]})
-    } else{
-      console.log("Not a Match !");
-    }
-  };
+  // for(let i = 0; i<posts.length; i++) {
+  //   const storedTitle = _.lowerCase(posts[i].title);
+  //
+  //   if (storedTitle === requestedTitle) {
+  //     // console.log("Match Found!");
+  //     res.render("post", {"postDetail": posts[i]})
+  //   } else{
+  //     console.log("Not a Match !");
+  //   }
+  // };
   // posts.foEach(function(post) {
   //
   // });
